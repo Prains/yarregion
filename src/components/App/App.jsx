c200b100fbe4ad059a06c2f25b49917b174199bd
Prains <sleskov2013@yandex.ru>
2023-05-13 12:15:03 +0300

    review

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import Footer from "./Footer/Footer.jsx"
+import "./App.css";
+import Footer from "./Footer/Footer.jsx";
 function App() {
-
   return (
     <div className="App">
       <Footer
-       address="г. Москва, улица Моховая, дом 10 строение 2"
-       phone="+7 (495) 568-14-70"
-       email="gubern@adm.yar.ru0"/>
+        address="г. Москва, улица Моховая, дом 10 строение 2"
+        phone="+7 (495) 568-14-70"
+        email="gubern@adm.yar.ru0"
+      />
+      {/* большое количество данных лучше передавать объектом. так код становится чище*/}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
