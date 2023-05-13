6c26f5d54be87b723188b6aa0270362b3832a214
Nastya <krasnyshova@icloud.com>
2023-05-13 16:05:57 +0300

    Footer Fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Footer from "./Footer/Footer.jsx";
 function App() {
+  const owner={
+    address:'г. Москва, улица Моховая, дом 10 строение 2',
+    phone:'+7 (495) 568-14-70',
+    email:'gubern@adm.yar.ru0'
+  }
   return (
     <div className="App">
       <Footer
-        address="г. Москва, улица Моховая, дом 10 строение 2"
-        phone="+7 (495) 568-14-70"
-        email="gubern@adm.yar.ru0"
-      />
-      {/* большое количество данных лучше передавать объектом. так код становится чище*/}
+        owner={owner}/>
+    
     </div>
   );
 }
--- src/components/App/Footer/Footer.jsx
+++ src/components/App/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from "./Footer.module.css";
-import Logo from "../../../images/Footer/Logo.jpg";
+import Logo from "../../../images/Footer/Logo.svg";
 
 function Footer(props) {
   return (
@@ -9,29 +9,31 @@ function Footer(props) {
           <a href="/">
             <img className={styles.footer_Logo} src={Logo} alt="Logo" />
           </a>
-        </div>{" "}
-        {/* ссылка на домашнюю страницу всегда /*/}
+        </div>
         <div className={styles.footer__info}>
           <p>Представительство Ярославской области</p>
-          <p>{props.address}</p>
-          <p>{props.phone}</p>
+          <p>{props.owner.address}</p>
+          <p>{props.owner.phone}</p>
           <div className={styles.footer__info_EmailAddress}>
-            <p>{props.email}</p>
+            <p>{props.owner.email}</p>
           </div>
         </div>
-        <div className={styles.footer__links_1}>
-          <a href="#">О регионе</a>
-          <a href="#">Предпринимателю</a>
-          <a href="#">Инвестору</a>
-          <a href="#">Для НКО</a>
+        <div className={styles.footer_links}>
+          <div className={styles.footer_links__grid}>
+            <div className={styles.footer_links__grid_left}>
+              <a href="#">О регионе</a>
+              <a href="#">Предпринимателю</a>
+              <a href="#">Инвестору</a>
+              <a href="#">Для НКО</a>
+            </div>
+            <div className={styles.footer_links__grid_right}>
+              <a href="#">Начинающему предпринимателю</a>
+              <a href="#">Инвестиционная карта</a>
+              <a href="#">Туристу</a>
+              <a href="#">Торги</a>
+            </div>
+          </div>
         </div>
-        <div className={styles.footer__links_2}>
-          <a href="#">Начинающему предпринимателю</a>
-          <a href="#">Инвестиционная карта</a>
-          <a href="#">Туристу</a>
-          <a href="#">Торги</a>
-        </div>
-        {/*последние два блока можно вынести в один компонент и переиспользовать */}
       </div>
     </div>
   );
