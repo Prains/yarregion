a4599492d64e6525c8167013b96d2e2c6a58b5b5
Nastya <krasnyshova@icloud.com>
2023-05-14 21:45:32 +0300

    The footer is ready

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,7 @@
 import "./App.css";
-import Footer from "./Footer/Footer.jsx";
+
 function App() {
-  const owner={
-    address:'г. Москва, улица Моховая, дом 10 строение 2',
-    phone:'+7 (495) 568-14-70',
-    email:'gubern@adm.yar.ru0'
-  }
-  return (
-    <div className="App">
-      <Footer
-        owner={owner}/>
-    
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
