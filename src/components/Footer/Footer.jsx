50ab39123f7122e568431403f0e92e27eab45eb0
Nastya <krasnyshova@icloud.com>
2023-05-14 22:48:17 +0300

    Footer +Cinematics

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 import { FooterLinks } from "./FooterLinks/FooterLinks";
 import FooterLogo from "./FooterLogo";
 
-function Footer({owner}) {
+function Footer({ owner }) {
   const FooterText1 = {
     links1: 'О регионе',
     links2: 'Предпринимателю',
@@ -16,25 +16,23 @@ function Footer({owner}) {
     links4: 'Торги'
   }
   return (
-    <div className={styles.footer}>
+    <footer className={styles.footer}>
       <div className={styles.footer__content}>
         <FooterLogo />
         <div className={styles.footer__info}>
           <p>Представительство Ярославской области</p>
           <p>{owner.address}</p>
           <p>{owner.phone}</p>
-          <div className={styles.footer__info_EmailAddress}>
-            <p>{owner.email}</p>
-          </div>
-        </div>
-        <div className={styles.footer_links}>
-          <div className={styles.footer__grid_container}>
-            <FooterLinks FooterText={FooterText1}/>
-            <FooterLinks FooterText={FooterText2}/>
-          </div>
+          <p className={styles.footer__info_Email}>{owner.email}</p>
         </div>
       </div>
-    </div>
+      <div className={styles.footer_links}>
+        <div className={styles.footer__grid_container}>
+          <FooterLinks FooterText={FooterText1} />
+          <FooterLinks FooterText={FooterText2} />
+        </div>
+      </div>
+    </footer>
   );
 }
 
--- src/components/Footer/FooterLinks/FooterLinks.jsx
+++ src/components/Footer/FooterLinks/FooterLinks.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import styles from './Footer.module.css'
 
-export const FooterLinks = ({FooterText}) => {
-    
+export const FooterLinks = ({ FooterText }) => {
+
   return (
-    <ul className={styles.links__grid_container}>
-              <li><a href="#">{FooterText.links1}</a></li>
-              <li><a href="#">{FooterText.links2}</a></li>
-              <li><a href="#">{FooterText.links3}</a></li>
-              <li><a href="#">{FooterText.links4}</a></li>
-            </ul>
+    <nav className={styles.links__grid_container}>
+      <ul>
+        <li><a href="#">{FooterText.links1}</a></li>
+        <li><a href="#">{FooterText.links2}</a></li>
+        <li><a href="#">{FooterText.links3}</a></li>
+        <li><a href="#">{FooterText.links4}</a></li>
+      </ul>
+    </nav>
   )
 }
