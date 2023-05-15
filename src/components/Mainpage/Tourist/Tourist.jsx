41199bc367e0e47136bdbcf4e1f4dc777b96aa82
realchayka <lonelyplay54@gmail.com>
2023-05-15 10:05:45 +0300

    Added Tourist section in Main Page v3, fixed bugs

--- src/components/Mainpage/Tourist/Tourist.jsx
+++ src/components/Mainpage/Tourist/Tourist.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './Tourist.module.scss'
 import {Swiper, SwiperSlide} from "swiper/react";
 
@@ -11,69 +10,81 @@ import "./TouristSwiper.scss";
 
 // import required modules
 import {Navigation, Pagination} from "swiper";
-import {sliderImageOne, touristImage} from "../../images/Main/Tourist/index.js";
+import {sliderImageOne, touristImage} from "../../../images/Main/Tourist/index.js";
 
 const Tourist = () => {
     const arrData = [
         {
             id: 14,
             img: sliderImageOne,
-            text: 'textone'
+            text: 'textone',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 13,
             img: sliderImageOne,
-            text: 'texttwo'
+            text: 'texttwo',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 10,
             img: sliderImageOne,
-            text: 'textthree'
+            text: 'textthree',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 11,
             img: sliderImageOne,
-            text: 'textfour'
+            text: 'textfour',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 1,
             img: sliderImageOne,
-            text: 'textone'
+            text: 'textone',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 2,
             img: sliderImageOne,
-            text: 'texttwo'
+            text: 'texttwo',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 3,
             img: sliderImageOne,
-            text: 'textthree'
+            text: 'textthree',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 4,
             img: sliderImageOne,
-            text: 'textfour'
+            text: 'textfour',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 5,
             img: sliderImageOne,
-            text: 'textone'
+            text: 'textone',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 6,
             img: sliderImageOne,
-            text: 'texttwo'
+            text: 'texttwo',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 7,
             img: sliderImageOne,
-            text: 'textthree'
+            text: 'textthree',
+            alt: 'На фотографии изображены горы'
         },
         {
             id: 8,
             img: sliderImageOne,
-            text: 'textfour'
+            text: 'textfour',
+            alt: 'На фотографии изображены горы'
         },
 
     ]
@@ -85,7 +96,7 @@ const Tourist = () => {
 
                 <div className={styles.textContainer}>
                     <div className={styles.mainContainer}>
-                        <img className={styles.touristImage} src={touristImage} alt="tourist"/>
+                        <img className={styles.touristImage} src={touristImage} alt="Путевая дорожка с символом локации в конце"/>
                         <div className={styles.textBlock}>
                             <p className={styles.text}>
                                 Ярославская область открыта для туристов. Наш край по праву считается одним из самых
@@ -106,7 +117,7 @@ const Tourist = () => {
                                     arrData.map(item =>
                                         <SwiperSlide key={item.id}>
                                             <a href="#">
-                                                <img className={styles.sliderImage} src={item.img} alt="slider-image"/>
+                                                <img className={styles.sliderImage} src={item.img} alt={item.alt}/>
                                                 <p className={styles.sliderText}>{item.text}</p>
                                             </a>
                                     </SwiperSlide>)
