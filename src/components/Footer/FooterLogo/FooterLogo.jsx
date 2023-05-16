304f36b3eda7aaf29f9b257371d76768cee8b987
Prains <sleskov2013@yandex.ru>
2023-05-16 19:29:54 +0300

    minor changes

--- src/components/Footer/FooterLogo/FooterLogo.jsx
+++ src/components/Footer/FooterLogo/FooterLogo.jsx
@@ -1,5 +1,5 @@
-import Logo from "../../images/Footer/Logo.svg";
-import styles from "./Footer.module.css";
+import Logo from "../../../images/Footer/Logo.svg";
+import styles from "../Footer.module.css";
 
 function FooterLogo() {
     return (
