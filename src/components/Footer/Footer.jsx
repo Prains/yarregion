d19934dc3e3f383bf6f0394163534b42e56cc479
Nastya <krasnyshova@icloud.com>
2023-05-17 21:10:43 +0300

    Fix mailto

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -45,7 +45,7 @@ function Footer({owner}) {
           <p>Представительство Ярославской области</p>
           <p>{owner.address}</p>
           <p>{owner.phone}</p>
-          <p className={styles.footer__info_Email}><a href='mailto:{owner.email}'>{owner.email}</a></p>
+          <p><a href='mailto:gubern@adm.yar.ru0'>{owner.email}</a></p>
         </div>
         <div className={styles.footer_links}>
           <nav className={styles.footer__grid_container}>
