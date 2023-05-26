11eb0ff13f6df14500fde752b21bafbcda898792
Artem <artem.glebov2004@yandex.ru>
2023-05-26 10:58:18 +0300

    delete contacts for App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,7 @@
-import Contacts from "../Mainpage/Contacts/Contacts";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Contacts />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
